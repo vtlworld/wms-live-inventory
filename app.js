5445cce290869c55a0b05b481cc73027f21f3f27
vtlworld <vtlworld.com@gmail.com>
2025-12-25 15:26:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+// js/app.js
+import { login } from "./auth.js";
 
+async function init() {
+  try {
+    // TEMP: use a read-only inventory user
+    await login("inventory_user@email.com", "password");
+    console.log("WMS login successful");
+  } catch (err) {
+    console.error(err.message);
+    alert("WMS login failed");
+  }
+}
+
+init();

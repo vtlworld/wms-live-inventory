8fe6e1b3ad3456af5f2325969092e5b2a47aaebf
vtlworld <vtlworld.com@gmail.com>
2025-12-25 15:50:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // js/config.js
 export const CONFIG = {
-  WMS_BASE_URL: "https://wms.mps-las.com/login", // change this
+  WMS_BASE_URL: "https://wms.mps-las.com", // change this
   REFRESH_INTERVAL_MS: 3 * 60 * 1000 // 3 minutes
 };

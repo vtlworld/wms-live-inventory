e14af0c4689861b01923da832547d2b9fcd056f9
vtlworld <vtlworld.com@gmail.com>
2025-12-26 19:22:11 +0530

    Update inbound.js

--- inbound.js
+++ inbound.js
@@ -1,1 +1,14 @@
+import { CONFIG } from "./config.js";
+import { getAuthHeaders } from "./auth.js";
 
+export async function fetchInbound() {
+  const res = await fetch(`${CONFIG.WMS_BASE_URL}/inbound/list`, {
+    method: "GET",
+    headers: getAuthHeaders()
+  });
+
+  if (!res.ok) throw new Error("Inbound fetch failed");
+
+  const json = await res.json();
+  return json.data || [];
+}

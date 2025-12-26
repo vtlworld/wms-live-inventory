5186811e74aab292778ed9663449f9b05fb7aa7c
vtlworld <vtlworld.com@gmail.com>
2025-12-26 17:00:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // js/config.js
 export const CONFIG = {
-  WMS_BASE_URL: "https://nkn4mwko4i.execute-api.ap-south-1.amazonaws.com/prod", // change this
+  WMS_BASE_URL: "https://wms-proxy.vtlworld-com.workers.dev/", // change this
   REFRESH_INTERVAL_MS: 3 * 60 * 1000 // 3 minutes
 };

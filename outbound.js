179a2ccdec1c87df49459c77b1986c2f6c611dbb
vtlworld <vtlworld.com@gmail.com>
2025-12-26 19:22:25 +0530

    Update outbound.js

--- outbound.js
+++ outbound.js
@@ -1,1 +1,15 @@
+import { CONFIG } from "./config.js";
+import { getAuthHeaders } from "./auth.js";
+
+export async function fetchOutbound() {
+  const res = await fetch(`${CONFIG.WMS_BASE_URL}/outbound/list`, {
+    method: "GET",
+    headers: getAuthHeaders()
+  });
+
+  if (!res.ok) throw new Error("Outbound fetch failed");
+
+  const json = await res.json();
+  return json.data || [];
+}
 
